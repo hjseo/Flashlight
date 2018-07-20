410df6c9d367d6b6ec665e8ad5f79c4723e22d77
hyejeong seo <seohj92@podbbang.com>
2018-07-20 12:10:31 +0900

    플래시 on, off

--- Flashlight/ViewController.swift
+++ Flashlight/ViewController.swift
@@ -39,10 +39,34 @@ class ViewController: UIViewController {
             // couldn't load file :(
         }
     }
+    
+    func toggleTorch(on: Bool) {
+        guard let device = AVCaptureDevice.default(for: .video) else { return }
+        
+        if device.hasTorch {
+            do {
+                try device.lockForConfiguration()
+                
+                if on == true {
+                    device.torchMode = .on
+                } else {
+                    device.torchMode = .off
+                }
+                
+                device.unlockForConfiguration()
+            } catch {
+                print("Torch could not be used")
+            }
+        } else {
+            print("Torch is not available")
+        }
+    }
+    
     @IBAction func switchTapped(_ sender: Any) {
         isOn = !isOn
         
         soundPlayer?.play()
+        toggleTorch(on: isOn)
         
         flashImageView.image = isOn ? #imageLiteral(resourceName: "onBG") : #imageLiteral(resourceName: "offBG")
         switchButton.setImage(isOn ? #imageLiteral(resourceName: "onSwitch") : #imageLiteral(resourceName: "offSwitch"), for: .normal)
